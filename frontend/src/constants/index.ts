126e055228a21f00bb53a7ea537b81e12a98610d
Gabriel <bielrj170@gmail.com>
2025-11-28 01:34:27 -0300

    feat: create new routes

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,10 +1,5 @@
-// Routes
 export { ROUTES } from "./routes";
 export type { Route } from "./routes";
-
-// Changelog
 export { CHANGELOG } from "./changelog";
-
-// Demo
 export { DEMO_CODE_LINES, MOCK_HISTORY } from "./demo";
 
