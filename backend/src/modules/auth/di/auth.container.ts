5dd574ce46edcd8b335c46ce072d774819e603aa
Gabriel <bielrj170@gmail.com>
2025-11-28 01:21:07 -0300

    feat: create new token encrypter in di

--- backend/src/modules/auth/di/auth.container.ts
+++ backend/src/modules/auth/di/auth.container.ts
@@ -3,16 +3,18 @@ import { CreateSessionUseCase } from "../use-cases/create-session.usecase";
 import { CreateUserUseCase } from "../use-cases/create-user.usecase";
 import { IAuthContract } from "../contract/auth-contract";
 import { BcryptHasher } from "../cryptography/bcryptjs";
+import { TokenEncrypter, getTokenEncrypter } from "../cryptography/token-encrypter";
 import { PrismaAuthRepository } from "../repository/prisma-auth.repository";
 import { prisma } from "../../../models/prisma.client";
 
 class AuthContainer {
   _repository: IAuthContract | null = null;
   _hashService: BcryptHasher | null = null;
+  _tokenEncrypter: TokenEncrypter | null = null;
 
   get repository(): IAuthContract {
     if (!this._repository) {
-      this._repository = new PrismaAuthRepository(prisma);
+      this._repository = new PrismaAuthRepository(prisma) as IAuthContract;
     }
     return this._repository;
   }
@@ -24,12 +26,19 @@ class AuthContainer {
     return this._hashService;
   }
 
+  get tokenEncrypter(): TokenEncrypter {
+    if (!this._tokenEncrypter) {
+      this._tokenEncrypter = getTokenEncrypter();
+    }
+    return this._tokenEncrypter;
+  }
+
   get createSessionUseCase(): CreateSessionUseCase {
     return new CreateSessionUseCase(this.repository, this.hashService);
   }
 
   get createUserUseCase(): CreateUserUseCase {
-    return new CreateUserUseCase(this.repository, this.hashService);
+    return new CreateUserUseCase(this.repository, this.hashService, this.tokenEncrypter);
   }
 }
 
