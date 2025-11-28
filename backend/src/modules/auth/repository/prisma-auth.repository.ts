f9637d9f2c1b2423b951beb9ab1e27f786c6fad3
Gabriel <bielrj170@gmail.com>
2025-11-28 01:22:05 -0300

    feat: create new functions in repository

--- backend/src/modules/auth/repository/prisma-auth.repository.ts
+++ backend/src/modules/auth/repository/prisma-auth.repository.ts
@@ -1,6 +1,6 @@
 import { PrismaClient, User } from "@prisma/client";
 import { IAuthContract } from "../contract/auth-contract";
-import { CreateUserDto } from "../dto/auth-dto";
+import { CreateUserDto, CreateUserResponseDto, CreateSessionUserDto, GetMeResponseDto, GetUserWithTokenDto } from "../dto/auth-dto";
 
 export class PrismaAuthRepository implements IAuthContract {
   constructor(private readonly prisma: PrismaClient) {}
@@ -9,7 +9,7 @@ export class PrismaAuthRepository implements IAuthContract {
     email,
     password,
     githubPersonalAccessToken,
-  }: CreateUserDto): Promise<User> {
+  }: CreateUserDto): Promise<CreateUserResponseDto> {
     const user = await this.prisma.user.create({
       data: {
         email,
@@ -18,30 +18,70 @@ export class PrismaAuthRepository implements IAuthContract {
       },
     });
 
-    return user;
+    return {
+      id: user.id,
+      email: user.email,
+      githubPersonalAccessToken: user.githubPersonalAccessToken || "",
+    };
   }
 
-  async getUserByEmail(email: string): Promise<User | null> {
+  async getUserByEmail(email: string): Promise<CreateSessionUserDto | null> {
     const user = await this.prisma.user.findUnique({
       where: { email },
     });
 
-    return user;
+    if (!user) {
+      return null;
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      passwordHash: user.passwordHash,
+      githubPersonalAccessToken: user.githubPersonalAccessToken || "",
+    };
   }
 
-  async getUserById(id: string): Promise<User | null> {
+  async getUserById(id: string): Promise<GetMeResponseDto | null> {
     const user = await this.prisma.user.findUnique({
       where: { id },
     });
 
-    return user;
+    if (!user) {
+      return null;
+    }
+
+    return {
+      id: user.id,
+    };
   }
 
-  async getMe(userId: string): Promise<User | null> {
+  async getMe(userId: string): Promise<GetMeResponseDto | null> {
     const user = await this.prisma.user.findUnique({
       where: { id: userId },
     });
 
-    return user;
+    if (!user) {
+      return null;
+    }
+
+    return {
+      id: user.id,
+    };
+  }
+
+  async getUserWithToken(userId: string): Promise<GetUserWithTokenDto | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return {
+      id: user.id,
+      githubPersonalAccessToken: user.githubPersonalAccessToken,
+    };
   }
 }
